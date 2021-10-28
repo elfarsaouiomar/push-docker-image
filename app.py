2a0a0fed20f19faab65a98db4011e925a3c13f35
omarelfarsaoui <omar.farsaoui@outlook.fr>
2021-10-28 16:51:40 +0100

    Update code

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Home Page\n'
+    return 'Home World\n'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8070, debug=False)
